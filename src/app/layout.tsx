c20b1fc75d13987358d43267b720ffc0755bb075
zhangolivia7 <zhangolivia7@gmail.com>
2026-07-27 22:00:30 -0700

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const lcd14 = localFont({
   variable: "--font-lcd14",
